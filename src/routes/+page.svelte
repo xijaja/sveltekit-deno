39e7ea5be4ba861fa4348ba85104052a9326677b
希嘉嘉 <hi@xijaja.com>
2023-05-29 15:17:52 +0800

    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <h1 class="text-4xl font-bold py-6">Welcome to SvelteKit</h1>
 
 <div class="space-x-4">
-	<button class="text-blue-700 underline underline-offset-4">首页</button>
-	<a href="/about" class="hover:text-blue-700">关于</a>
+	<button class="text-blue-700 underline underline-offset-4">index</button>
+	<a href="/about" class="hover:text-blue-700">about</a>
 </div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,6 @@
 <h1 class="text-4xl font-bold py-6">Welcome to SvelteKit</h1>
 
 <div class="space-x-4">
-	<a href="/" class="hover:text-blue-700">首页</a>
-	<button class="text-blue-700 underline underline-offset-4">关于</button>
+	<a href="/" class="hover:text-blue-700">index</a>
+	<button class="text-blue-700 underline underline-offset-4">about</button>
 </div>
