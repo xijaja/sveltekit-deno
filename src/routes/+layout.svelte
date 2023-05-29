a73f4e76ef56d43f6237a765f178a20d835edd6d
希嘉嘉 <hi@xijaja.com>
2023-05-29 15:11:37 +0800

    update

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import "../app.postcss";
+</script>
+
+<main class="mx-auto max-w-4xl">
+  <slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-4xl font-bold py-6">Welcome to SvelteKit</h1>
+
+<div class="space-x-4">
+	<button class="text-blue-700 underline underline-offset-4">首页</button>
+	<a href="/about" class="hover:text-blue-700">关于</a>
+</div>
